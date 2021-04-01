314deadc0c2d18c56099afd6e6746b3e3acb00e2
Kavyashree <69260342+kavyashree-205@users.noreply.github.com>
2021-04-02 00:16:11 +0530

    Update insertion_sort.py

--- insertion_sort.py
+++ insertion_sort.py
@@ -3,11 +3,11 @@ def insertion_sort(a):
     for i in range(1,n):
         current_element=a[i]
         j=i-1
-        while current_element<a[j] and j>=0:
+        while (current_element<a[j] and j>=0):
             a[j+1]=a[j]
             j=j-1
         a[j+1]=current_element
     return a
 if __name__=="__main__":
-    a=[int(x) for x  in input().split()]
+    a=[int(x) for x in input().split()]
     print(insertion_sort(a))
